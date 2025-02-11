fc3358b1102263c963f38a2c157415152b4b69c2
Daniel Hristov <dhristov06@gmail.com>
2025-02-11 21:28:54 +0200

    thread creation part 1
    i really hate this shit

---
+++ Dev/Web/Dev.Web/Controllers/ThreadController.cs
@@ -0,0 +1,20 @@
+﻿using Dev.Web.Models.Thread;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dev.Web.Controllers
+{
+    public class ThreadController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateConfirm(CreateThreadModel model)
+        {
+            return View();
+        }
+    }
+}
